ea0b6f13629cc682fb6cd81216adce4533e11d91
chris_park <jongikp90@gmail.com>
2017-08-02 22:49:22 +0900

    0802 second commit

--- 0802classnote.R
+++ 0802classnote.R
@@ -1,2 +1,8 @@
 install.packages("rmarkdown")
 library(rmarkdown)
+
+devtools::install_github("bnosac/cronR")
+
+install.packages('miniUI')
+install.packages('shiny')
+install.packages('shinyFiles')
